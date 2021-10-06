b4c80e8bb6e93798b9d6b051233447ef6bbfbe7a
Bishram <m.bishram.yashir@gmail.com>
2021-10-06 13:16:42 +0800

    Adding index and login page test setup

---
+++ src/pages/index.spec.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+describe("Home Page", () => {
+	it.todo("should do something");
+});
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,9 @@
 import Head from "next/head";
-import MainLayout from "../layouts/MainLayout/MainLayout";
+import { MainLayout } from "@/layouts/MainLayout/MainLayout";
 
 export default function Login() {
 	return (
-		<MainLayout>
+		<MainLayout disableNav>
 			<Head>
 				<title>
 					Login - {process.env.NEXT_PUBLIC_APPLICATION_NAME}
