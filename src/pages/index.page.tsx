18d2c2a5133c67acf2246c2445efbe2c27bd49f9
Bishram <m.bishram.yashir@gmail.com>
2021-10-10 16:53:45 +0800

    Adding primary and secondary color scheme and changing button styling

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/Button/Button";
+import { Input } from "@/components/ui/Input/Input";
+
 export default function Index() {
-	return <div>Index</div>;
+	return (
+		<div>
+			Index
+			<Button color="primary">Test</Button>
+			<Input fullWidth placeholder="test" />
+		</div>
+	);
 }
