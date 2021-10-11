852593b774cba39977bdadf716c2093f3fc1e700
Bishram <m.bishram.yashir@gmail.com>
2021-10-11 17:18:29 +0800

    Fix random bg test

--- src/utils/random-bg.spec.ts
+++ src/utils/random-bg.spec.ts
@@ -1,7 +1,7 @@
 import { randomBg } from "@/utils/random-bg";
 
 describe("RandomBg", () => {
-	it("should return string with 'bg-{color}-500' pattern", () => {
-		expect(randomBg()).toMatch(/^bg-.+-500$/g);
+	it("should return string with 'bg-{color}-[678]00' pattern", () => {
+		expect(randomBg()).toMatch(/^bg-.+-[678]00$/g);
 	});
 });
