39ced29cccea2d761eeb5aa5d4ff5f0ce727bbc2
Bishram <m.bishram.yashir@gmail.com>
2021-10-11 16:24:21 +0800

    Removing login css file

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -1,14 +1,17 @@
 import Head from "next/head";
 import { MainLayout } from "@/layouts/MainLayout/MainLayout";
 import { Typography } from "@/components/ui/Typography/Typography";
-import style from "./login.module.css";
 import { Quote } from "@/components/ui/Quote/Quote";
 import { QuoteModel } from "@/model/quote-model";
 import { LoginForm } from "@/components/login/Form/Form";
 
 export default function Login() {
 	return (
-		<MainLayout className={style.wrapper} classMain={style.main} disableNav>
+		<MainLayout
+			className="max-w-screen-xl"
+			classMain="grid md:grid-cols-2 gap-8 items-center content-center"
+			disableNav
+		>
 			<Head>
 				<title>
 					Login - {process.env.NEXT_PUBLIC_APPLICATION_NAME}
