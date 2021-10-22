d6dfed927fe22a92651fee32732e79581c798ee0
Bishram <m.bishram.yashir@gmail.com>
2021-10-22 22:19:45 +0800

    Create test on quotes api

--- src/pages/api/quotes/index.spec.ts
+++ src/pages/api/quotes/index.spec.ts
@@ -1,7 +1,42 @@
 import quoteApi from "./index.page";
+import { NextApiRequest, NextApiResponse } from "next";
+import {
+	getAllQuotes,
+	createQuotes,
+	invalidMethod,
+} from "@/libs/mongodb/fetcher";
+
+jest.mock("@/libs/mongodb/fetcher", () => ({
+	getAllQuotes: jest.fn(),
+	createQuotes: jest.fn(),
+	invalidMethod: jest.fn(),
+}));
 
 describe("API Quotes", () => {
 	it("should be able to separate method based on request method", () => {
-		console.log(quoteApi);
+		const reqGet = {
+			method: "GET",
+		} as NextApiRequest;
+		const resGet = {} as NextApiResponse;
+		quoteApi(reqGet, resGet);
+		expect(getAllQuotes).toBeCalled();
+
+		const reqPost = {
+			method: "POST",
+		} as NextApiRequest;
+		const resPost = {} as NextApiResponse;
+		quoteApi(reqPost, resPost);
+		expect(createQuotes).toBeCalled();
+
+		const reqInvalid = {
+			method: "TEST",
+		} as NextApiRequest;
+		const resInvalid = {} as NextApiResponse;
+		quoteApi(reqInvalid, resInvalid);
+		expect(invalidMethod).toBeCalled();
+
+		expect(getAllQuotes).toBeCalledTimes(1);
+		expect(createQuotes).toBeCalledTimes(1);
+		expect(invalidMethod).toBeCalledTimes(1);
 	});
 });
