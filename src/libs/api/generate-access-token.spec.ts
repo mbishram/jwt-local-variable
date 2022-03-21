7f6daea1f4dac9498de2b307fd0fb78b017afa25
Bishram <m.bishram.yashir@gmail.com>
2022-03-21 18:17:40 +0800

    Add function to generate access token

---
+++ src/libs/api/generate-access-token.spec.ts
@@ -0,0 +1,38 @@
+import { generateAccessToken } from "@/libs/api/generate-access-token";
+import { UserModel } from "@/models/user-model";
+import { checkAuth } from "@/libs/api/check-auth";
+import { mockAPIArgs } from "@specs-utils/mock-api-args";
+
+describe("Generate Access Token", () => {
+	const data = new UserModel({
+		name: "Test",
+		username: "test",
+		email: "email@test.com",
+	});
+
+	it("should return nothing on error", async () => {
+		// To make it error
+		const SECRET = process.env.ACCESS_TOKEN_SECRET_KEY;
+		delete process.env.ACCESS_TOKEN_SECRET_KEY;
+
+		const token = await generateAccessToken(data);
+		expect(token).toBeFalsy();
+
+		// Return it
+		process.env.ACCESS_TOKEN_SECRET_KEY = SECRET;
+	});
+
+	it("should be able to generate access token", async () => {
+		const token = await generateAccessToken(data);
+		const authorization = "Bearer " + token;
+
+		const { req, res } = mockAPIArgs({
+			body: data,
+			headers: { authorization },
+		});
+
+		const [success, error] = await checkAuth(req, res);
+		expect(success).toBeTruthy();
+		expect(error).toBeFalsy();
+	});
+});
---
+++ src/libs/api/generate-access-token.ts
@@ -0,0 +1,18 @@
+import { UserModel } from "@/models/user-model";
+import jwt from "jsonwebtoken";
+
+/**
+ * Generate Access Token
+ * @param userData
+ */
+export async function generateAccessToken(userData: UserModel) {
+	try {
+		return jwt.sign(
+			{ ...userData },
+			process?.env?.ACCESS_TOKEN_SECRET_KEY as string,
+			{ expiresIn: "10m" }
+		);
+	} catch (error) {
+		return;
+	}
+}
