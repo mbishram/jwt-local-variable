0cf0c5cc48e7649d1c8c589f8d7eec6bb6ddf724
Bishram <m.bishram.yashir@gmail.com>
2022-06-19 00:51:05 +0800

    Make quote component susceptible to XSS

--- src/components/ui/Quote/Quote.tsx
+++ src/components/ui/Quote/Quote.tsx
@@ -2,6 +2,7 @@ import { HTMLProps } from "react";
 import { QuoteModel } from "@/models/quote-model";
 import styles from "./Quote.module.css";
 import clsx from "clsx";
+import InnerHTML from "dangerously-set-html-content";
 
 /**
  * Component to showcase a quote.
@@ -18,12 +19,9 @@ export function Quote({
 			className={clsx(styles.article, data?.bgColor || "bg-gray-800")}
 			{...props}
 		>
-			<p
-				className={styles.quote}
-				dangerouslySetInnerHTML={{
-					__html: data?.quote || "Quote kosong!",
-				}}
-			/>
+			<p className={styles.quote}>
+				<InnerHTML html={data?.quote || "Quote kosong!"} />
+			</p>
 			<p className={styles.name}>- {data?.name || "Anonymous"}</p>
 		</article>
 	);
