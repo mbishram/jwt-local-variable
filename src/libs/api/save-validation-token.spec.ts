ab4a596236f574b6ffb826460945991e533bc5c4
Bishram <m.bishram.yashir@gmail.com>
2022-08-06 23:41:20 +0800

    Update save validation token to include userId

--- src/libs/api/save-validation-token.spec.ts
+++ src/libs/api/save-validation-token.spec.ts
@@ -1,18 +1,21 @@
 import { saveValidationToken } from "@/libs/api/save-validation-token";
 import { TOKENS_COLLECTION_NAME } from "@/libs/api/is-token-valid";
 import { connectToDatabase } from "@/libs/mongodb/setup";
+import { ObjectId } from "bson";
 
 describe("Save Validation Token", () => {
 	it("should create new document on tokens collection when accessToken is passed", async () => {
 		let { db } = await connectToDatabase();
 
 		const token = "TestToken";
-		expect(await saveValidationToken(token)).toBeTruthy();
+		const userId = new ObjectId();
+		expect(await saveValidationToken(token, userId)).toBeTruthy();
 
 		const tokenRes = await db
 			.collection(TOKENS_COLLECTION_NAME)
 			.findOne({ token });
 
 		expect(tokenRes?.token).toBe(token);
+		expect(tokenRes?.userId).toEqual(userId);
 	});
 });
--- src/libs/api/save-validation-token.ts
+++ src/libs/api/save-validation-token.ts
@@ -1,19 +1,24 @@
 import { connectToDatabase } from "@/libs/mongodb/setup";
 import { TOKENS_COLLECTION_NAME } from "@/libs/api/is-token-valid";
 import crypto from "crypto";
+import { ObjectId } from "bson";
 
 /**
  * Save token and validationToken to db.
  * @param accessToken {string}
+ * @param userId {string}
  * @return {string | undefined}
  */
-export async function saveValidationToken(accessToken: string) {
+export async function saveValidationToken(
+	accessToken: string,
+	userId: ObjectId
+) {
 	try {
 		let { db } = await connectToDatabase();
 		const validationToken = crypto.randomBytes(32).toString("hex");
 		await db
 			.collection(TOKENS_COLLECTION_NAME)
-			.insertOne({ token: accessToken, validationToken });
+			.insertOne({ token: accessToken, validationToken, userId });
 
 		return validationToken;
 	} catch (e) {
