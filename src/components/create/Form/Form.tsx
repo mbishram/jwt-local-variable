bb8ab76426270e82092610a0151df8ef3aeec6f3
Bishram <m.bishram.yashir@gmail.com>
2022-09-30 17:44:35 +0800

    Accept query on create quote form

--- src/components/create/Form/Form.tsx
+++ src/components/create/Form/Form.tsx
@@ -4,6 +4,7 @@ import { HTMLProps, ReactNode, useEffect, useState } from "react";
 import { CREATE_INPUT_ATTR, CreateQuoteFormType } from "@/forms/create";
 import { randomBg } from "@/libs/random-bg";
 import { createQuote } from "@/libs/fetchers/quotes";
+import { useRouter } from "next/router";
 
 /**
  * To separate create quote logic
@@ -15,12 +16,24 @@ export function CreateForm({
 	className,
 	beforeForm,
 }: Pick<HTMLProps<HTMLFormElement>, "className"> & Props) {
+	const { query } = useRouter();
 	const [timer, setTimer] = useState(setTimeout(() => {}));
-	const initialValues = {
+	const [initialValues, setInitialValues] = useState({
 		name: "",
 		quote: "",
 		bgColor: randomBg(),
-	};
+	});
+
+	useEffect(() => {
+		const { name, quote } = query;
+		if (name || quote) {
+			setInitialValues({
+				...initialValues,
+				name: decodeURI((name as string) || ""),
+				quote: decodeURI((quote as string) || ""),
+			});
+		}
+	}, [query]);
 
 	const handleSubmit: FormikHandleSubmit<CreateQuoteFormType> = async (
 		values,
@@ -60,6 +73,7 @@ export function CreateForm({
 				initialValues={initialValues}
 				onSubmit={handleSubmit}
 				className={className}
+				enableReinitialize
 			/>
 		</>
 	);
